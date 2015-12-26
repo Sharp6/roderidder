494089150cea7dd04737ea3259843633d3c794e7
Philip Huysmans <philip.huysmans@gmail.com>
2015-12-26 19:50:15 +0100

    late updates

--- public/javascripts/roderidder.js
+++ public/javascripts/roderidder.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-
 	function rrModel() {
 		var self = this;
 		self.titel = ko.observable('Roodje');
@@ -8,86 +7,89 @@ $(document).ready(function() {
 		self.maxPageNr = ko.observable(1);
 
 		self.updateNavTabs = function(whichOne) {
-			$('ul.nav-tabs li.active').removeClass('active');
-  	  $('#'+whichOne).addClass('active');
+		  $('ul.nav-tabs li.active').removeClass('active');
+  	      $('#'+whichOne).addClass('active');
 		};
 
 		self.loadMaxPageNr = function() {
 			return $.ajax({
-    	  url: '/roderidder/api/numberOfPages',
-    		type: 'get'
-    	}).promise();
+    	       url: '/roderidder/api/numberOfPages',
+    	       type: 'get'
+            }).promise();
 		};
 
 		self.loadTitles = function() {
-      return $.ajax({
-    	  url: '/roderidder/api/titles/pages?pageNumber='+self.pageNumber(),
+          return $.ajax({
+    	    url: '/roderidder/api/titles/pages?pageNumber='+self.pageNumber(),
     		type: 'get'
-    	}).promise();
-    };
+    	  }).promise();
+        };
 
-    self.updateTitles = function() {
-    	self.updateNavTabs('collectieTab');
-    	
+        self.updateTitles = function() {
+            self.updateNavTabs('collectieTab');
+            self.loadMaxPageNr().then(function(data) {
+                self.maxPageNr(data);
+            });
 
-    	self.loadMaxPageNr().then(function(data) {
-    		self.maxPageNr(data);
-    	});
-
-    	self.loadTitles().then(function(data) {
-			  self.titles(data);
-		  });	
-    };
-
-    self.loadWhishlist = function() {
-      return $.ajax({
-    	  url: '/roderidder/api/whishlist',
-    		type: 'get'
-    	}).promise();
-    };
-
-    self.updateWhishlist = function() {
-    	self.updateNavTabs('whishlistTab');
-    	self.pageNumber(1);
-    	self.maxPageNr(1);
-    	self.loadWhishlist().then(function(data) {
-			  self.titles(data);
-		  });	
-    };
-
-    self.updateDubbel = function() {
-    	self.updateNavTabs('dubbelTab');
-    	self.pageNumber(1);
-    	self.maxPageNr(1);
-    	self.loadDubbel().then(function(data) {
-			  self.titles(data);
-		  });	
-    };
-
-    self.loadDubbel = function() {
-      return $.ajax({
-    	  url: '/roderidder/api/dubbel',
-    		type: 'get'
-    	}).promise();
-    };
-
-    
+            self.loadTitles().then(function(data) {
+                self.titles(data);
+            });	
+        };
 
 
+        self.loadWhishlist = function() {
+            return $.ajax({
+                url: '/roderidder/api/whishlist',
+                type: 'get'
+            }).promise();
+        };
 
-		self.increasePage = function() {
-			self.pageNumber(self.pageNumber() + 1);
-		};
+        self.updateWhishlist = function() {
+            self.updateNavTabs('whishlistTab');
+            self.pageNumber(1);
+           	self.maxPageNr(1);
+            self.loadWhishlist().then(function(data) {
+                self.titles(data);
+            });	
+        };
 
-		self.decreasePage = function() {
-			self.pageNumber(self.pageNumber() - 1);
-		};
+        self.updateDubbel = function() {
+            self.updateNavTabs('dubbelTab');
+            self.pageNumber(1);
+            self.maxPageNr(1);
+            self.loadDubbel().then(function(data) {
+                self.titles(data);
+            });	
+        };
 
-		self.pageNumber.subscribe(function(newVal) {
-			self.updateTitles();
-		});
+        self.loadDubbel = function() {
+            return $.ajax({
+                url: '/roderidder/api/dubbel',
+                type: 'get'
+            }).promise();
+        };
 
-		self.updateTitles();
-	}
-	ko.applyBindings(new rrModel());
+        self.increasePage = function() {
+            self.pageNumber(self.pageNumber() + 1);
+        };
+
+        self.decreasePage = function() {
+            self.pageNumber(self.pageNumber() - 1);
+        };
+
+        self.loadImage = function() {
+            return $.ajax({
+                url: 'https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=rode%20ridder%20244%20site%3Awpg.be',
+                type: 'get'
+            }).promise();
+        };
+
+        self.pageNumber.subscribe(function(newVal) {
+            self.updateTitles();
+        });
+	   self.updateTitles();
+    }
+
+    ko.applyBindings(new rrModel());
+
 });
